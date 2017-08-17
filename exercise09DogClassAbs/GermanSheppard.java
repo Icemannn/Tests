dd2c2dbba5b97f09ea03febe9f091f5deb56f9df
Andrei <danandreist@gmail.com>
2017-08-17 20:11:30 +0300

    fixed toString() not showing...

---
+++ exercise09DogClassAbs/GermanSheppard.java
@@ -0,0 +1,11 @@
+package exercise09DogClassAbs;
+
+/**
+ * Created by me on 8/17/2017.
+ */
+public class GermanSheppard extends Dog {
+    public static String anotherAttribute;
+    public GermanSheppard(String name, String owner, int age) {
+        super(name, owner, age);
+    }
+}
